9c0580a0e05f1c0868dac686a11588d1d0e53122
kenter.zheng <972990430@qq.com>
2019-09-07 11:39:23 +0800

    修改API环境变量

--- src/models/environment.js
+++ src/models/environment.js
@@ -1,3 +1,3 @@
 export class environment {
-  static API_URL = process.env.VUE_APP_API_URL || 'http://localhost:4100'
+  static API_URL = process.env.VUE_APP_API_URL || 'http://39.108.121.182:4100' || 'http://localhost:4100'
 }
